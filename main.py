d48e2793b16cce76f8d57794d5a4dca2838f1cac
dan <brazhnykdan@gmail.com>
2024-02-15 22:30:14 +0200

    enhance dev1

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ n1 = number // 100
 n2 = number // 10 % 10
 n3 = number % 10
 maxdigit = 0
-mindigit =0
+mindigit = 0
 if n1 >= n2 and n1 >= n3:
     maxdigit = n1
 if n2 >= n1 and n2 >= n3:
     maxdigit = n2
 if n3 > n1 and n3 > n2:
     maxdigit = n3
-if n1<=n2 and n1<=n3:
+if n1 <= n2 and n1 <= n3:
     mindigit = n1
-if n2<=n1 and n2<=n3:
+if n2 <= n1 and n2 <= n3:
     mindigit = n2
-if n3<=n1 and n3<=n2:
+if n3 <= n1 and n3 <= n2:
     mindigit = n3
 
 average = (n1 + n2 + n3) / 3
